fb94a59dc5127a47650baf6193134f7685a59b91
Jochen Keil <jochen.keil@gmail.com>
2015-05-23 13:20:00 +0200

    Draw clock in multiple layers

--- paperjs/clock.js
+++ paperjs/clock.js
@@ -3,9 +3,8 @@ function toRad(angle)
   return angle / 180.0 * Math.PI;
 }
 
-function drawArc(center, angle, radius)
+function arcPath(center, angle, radius)
 {
-
   var rad         = toRad(angle);
       from_rel    = new Point(0, -radius);
       through_rel = new Point(Math.sin(rad / 2) * radius, -Math.cos(rad / 2) * radius);
@@ -15,208 +14,228 @@ function drawArc(center, angle, radius)
       through_abs = new Point(center) + through_rel;
       to_abs      = new Point(center) + to_rel;
 
-  return new Path.Arc(from_abs,
-                      through_abs,
-                      to_abs);
+  return { from: from_abs, through: through_abs, to: to_abs };
 }
 
-// // Create a Paper.js Path to draw a line into it:
-// var path = new Path();
-// // Give the stroke a color
-// path.strokeColor = 'black';
-// var start = new Point(100, 100);
-// // Move to start and draw a line from there
-// path.moveTo(start);
-// // Note the plus operator on Point objects.
-// // PaperScript does that for us, and much more!
-// path.lineTo(start + [ 100, -50 ]);
+var center = new Point(250, 250);
 
-// var path = new Path.Circle({
-//   center: view.center,
-//   radius: 30,
-//   strokeColor: 'black'
-// });
+var opacity = 0.25;
 
-// var path = new Path.Arc({
-//     from: [20, 20],
-//     through: [60, 20],
-//     to: [80, 80],
-//     strokeColor: 'black'
-// });
+var hor_radius = 175;
+var min_radius = 149;
+var sec_radius = 133;
 
-// function onFrame(event)
-// {
-//   // document.getElementById('test').innerHTML  = event.count;
-//   // document.getElementById('test').innerHTML += '<br>';
-//   // document.getElementById('test').innerHTML += event.time;
-//   // document.getElementById('test').innerHTML += '<br>';
-//   // document.getElementById('test').innerHTML += event.delta;
-// }
-
-/*
-var path_obj = {
-  from: [0, 0],
-  through: [Math.sin(Math.PI / 4) * 100, 100 - Math.cos(Math.PI / 4) * 100],
-  to: [100, 100],
-  strokeColor: 'red'
-};
-
-var circle = new Path.Circle(path_obj.through, 3);
-circle.strokeColor = 'green';
-*/
-
-// path = new Path.Arc(path_obj);
-
-// path = new Path.Arc({
-//   from: [0, 0],
-//   through: [50,50],
-//   to: [0, 100],
-//   strokeColor: 'black'
-// });
-
-// path = new Path.Arc({
-//   from: [0, 0],
-//   through: [Math.sin(1) * 50, Math.cos(1) * 50],
-//   to: [50, 50],
-//   strokeColor: 'red'
-// });
-
-// console.log('from: ' + path_obj.from + '; to: ' + path_obj.to + '; through: ' + path_obj.through);
-// console.log('sin(Math.PI / 4): ' + Math.sin(Math.PI / 4) + '; cos(Math.PI / 4): ' + Math.cos(Math.PI / 4));
-
-// drawArc(new Point(100, 100), 90, 100);
-// drawArc(new Point(157, 157), 90, 123);
-// // drawArc(new Point(57, 57), 123, 44);
-// // drawArc(new Point(50, 70), 180, 60);
-// drawArc(new Point(70, 70), 350, 60);
-// // path.strokeColor = 'red';
-
-var center = new Point(175, 200);
-
-var hor_radius = 100;
-var min_radius = 120;
-var sec_radius = 140;
-
-var hor_strokewidth = 20;
+var hor_strokewidth = 30;
 var min_strokewidth = 20;
-var sec_strokewidth = 20;
+var sec_strokewidth = 10;
 
-// var hor_path = drawArc(center, 0, hor_radius);
-// var min_path = drawArc(center, 0, min_radius);
-// var sec_path = drawArc(center, 0, sec_radius);
+var bg_circle = new Path.Circle(center, sec_radius + sec_strokewidth / 2 + 10);
+bg_circle.fillColor = '#f0f0f0';
+bg_circle.opacity = 0.5;
 
-// view.viewSize = new Size(400, 400);
+// TODO: hor layer, min layer, sec layer
+
+var hor_circle = new Path.Arc(arcPath(center, 360, hor_radius));
+hor_circle.opacity = opacity;
+hor_circle.strokeWidth = hor_strokewidth;
+hor_circle.strokeColor = '#8f8f8f';
+
+var min_circle = new Path.Arc(arcPath(center, 360, min_radius));
+min_circle.opacity = opacity;
+min_circle.strokeWidth = min_strokewidth;
+min_circle.strokeColor = '#8f8f8f';
+
+var sec_circle = new Path.Arc(arcPath(center, 360, sec_radius));
+sec_circle.opacity = opacity;
+sec_circle.strokeWidth = sec_strokewidth;
+sec_circle.strokeColor = '#8f8f8f';
+
+var base_layer = new Layer([bg_circle, hor_circle, min_circle, sec_circle]);
+
+var hands_layer = new Layer();
+
+var hor_layer = new Layer();
+var min_layer = new Layer();
+var sec_layer = new Layer();
+
+var hour = null;
+var minute = 59;
+var second = 57;
+
+function hand(options)
+{
+  var center      = options.center;
+      angle       = options.angle;
+      radius      = options.radius;
+      strokeWidth = options.strokeWidth;
+      strokeColor = options.strokeColor;
+      headColor   = options.headColor;
+      arcColor    = options.arcColor;
+      tailColor   = options.tailColor;
+      textColor   = options.textColor;
+      textContent = options.textContent;
+      fontSize    = options.fontSize;
+      fontFamily  = options.fontFamily;
+
+  var path = arcPath(center, angle, radius);
+
+  // var head = new Path.Circle(path.to, strokeWidth / 2);
+  // head.fillColor = headColor;
+  // head.strokeWidth = 2;
+  // head.strokeColor = arcColor;
+
+  var arc = new Path.Arc(path);
+  arc.strokeWidth = strokeWidth;
+  arc.strokeColor = arcColor;
+
+  // var tail = new Path.Circle(path.from, strokeWidth / 2);
+  // tail.fillColor = tailColor;
+
+  // var text = new PointText(path.to);
+  // text.content = textContent;
+  // text.fillColor = textColor;
+  // text.fontSize = fontSize;
+  // text.fontFamily = fontFamily;
+  // text.justification = 'center';
+  // text.position = path.to;
+
+  // return { head: head, arc: arc, tail: tail, text: text };
+  // return { head: head, arc: arc, text: text };
+  // return { head: head, arc: arc };
+  return { arc: arc };
+}
 
 setInterval(function() {
 
   var date = new Date();
-  var hours   = ('00' + date.getHours()).slice(-2);
-  var minutes = ('00' + date.getMinutes()).slice(-2);
-  var seconds = ('00' + date.getSeconds()).slice(-2);
 
-  // document.getElementById('test').innerHTML = hours + ':' + minutes + ':' + seconds;
+  // var cur_hour = 0;
+  // var cur_minute = 0;
+  // var cur_second = 0;
 
-  project.clear();
+  var cur_hour = date.getHours();
+  var cur_minute = date.getMinutes();
+  // var cur_minute = (minute + 1) % 60;
+  var cur_second = date.getSeconds();
+  // var cur_second = (second + 1) % 60;
 
-  /*
-  var hor_circle = new Path.Circle(center, hor_radius - hor_strokewidth / 2);
-  hor_circle.strokeColor = 'black';
+  if (cur_hour != hour) {
+    console.log('cur_hour != hour');
 
-  var min_circle = new Path.Circle(center, min_radius - min_strokewidth / 2);
-  min_circle.strokeColor = 'black';
+    if (cur_hour == 0) {
+      cur_hour = 24;
+    }
 
-  var sec_circle = new Path.Circle(center, sec_radius - sec_strokewidth / 2);
-  sec_circle.strokeColor = 'black';
-  */
+    hour = cur_hour;
+    var hours = ('00' + date.getHours()).slice(-2);
+    var hor_angle = (cur_hour % 12 / 12) * 360;
 
-  var hor_angle = (date.getHours() / 24) * 360;
-      min_angle = (date.getMinutes() / 60) * 360;
-      sec_angle = (date.getSeconds() / 60) * 360;
+    var hor_options =
+        { center      : center
+        , angle       : hor_angle
+        , radius      : hor_radius
+        , strokeWidth : hor_strokewidth
+        , strokeColor : '#FFBA00'
+        , headColor   : '#997000'
+        , arcColor    : '#FFBA00'
+        , tailColor   : '#FFBA00'
+        , textColor   : '#FFBA00'
+        , textContent : hours
+        , fontSize    : '20'
+        , fontFamily  : 'Ubuntu Light'
+        };
 
-  if (hor_angle == 0) {
-    hor_angle = 360;
+    var hor_hand = hand(hor_options);
+
+    hor_layer.removeChildren();
+    hor_layer.addChildren(
+        // [hor_hand.arc, hor_hand.tail, hor_hand.head, hor_hand.text]);
+        // [hor_hand.arc, hor_hand.head, hor_hand.text]);
+        // [hor_hand.arc, hor_hand.head]);
+        [hor_hand.arc]);
   }
 
-  if (min_angle == 0) {
-    min_angle = 360;
+  if (cur_minute != minute) {
+    console.log('cur_minute != minute');
+
+    if (cur_minute == 0) {
+      cur_minute = 60;
+    }
+
+    minute = cur_minute;
+    var minutes = ('00' + date.getMinutes()).slice(-2);
+    var min_angle = (cur_minute / 60) * 360;
+
+    var min_options =
+        { center      : center
+        , angle       : min_angle
+        , radius      : min_radius
+        , strokeWidth : min_strokewidth
+        , strokeColor : '#8EBB00'
+        , headColor   : '#557000'
+        , arcColor    : '#8EBB00'
+        , tailColor   : '#8EBB00'
+        , textColor   : '#8EBB00'
+        , textContent : minutes
+        , fontSize    : '20'
+        , fontFamily  : 'Ubuntu Light'
+        };
+
+    var min_hand = hand(min_options);
+
+    min_layer.removeChildren();
+    min_layer.addChildren(
+        // [min_hand.arc, min_hand.tail, min_hand.head, min_hand.text]);
+        // [min_hand.arc, min_hand.head, min_hand.text]);
+        // [min_hand.arc, min_hand.head]);
+        [min_hand.arc]);
   }
 
-  if (sec_angle == 0) {
-    sec_angle = 360;
+  if (cur_second != second) {
+
+    if (cur_second == 0) {
+      cur_second = 60;
+    }
+
+    // console.log('cur_second != second: ' + seconds);
+
+    // if (cur_second == 0) {
+    //   second = null;
+    // }
+
+    second = cur_second;
+    var seconds = ('00' + cur_second).slice(-2);
+    var sec_angle = (second / 60) * 360;
+
+    var sec_options =
+        { center      : center
+        , angle       : sec_angle
+        , radius      : sec_radius
+        , strokeWidth : sec_strokewidth
+        , strokeColor : '#00A9EF'
+        , headColor   : '#00658F'
+        , arcColor    : '#00A9EF'
+        , tailColor   : '#00A9EF'
+        , textColor   : '#00A9EF'
+        , textContent : seconds
+        , fontSize    : '20'
+        , fontFamily  : 'Ubuntu Light'
+        };
+
+    var sec_hand = hand(sec_options);
+
+    sec_layer.removeChildren();
+    sec_layer.addChildren(
+        // [sec_hand.arc, sec_hand.tail, sec_hand.head, sec_hand.text]);
+        // [sec_hand.arc, sec_hand.head, sec_hand.text]);
+        // [sec_hand.arc, sec_hand.head]);
+        [sec_hand.arc]);
   }
 
-//   var hor_path = drawArc(center, hor_angle, hor_radius);
-//   hor_path.strokeWidth = hor_strokewidth;
-//   hor_path.strokeColor = 'red';
-//
-//   var min_path = drawArc(center, min_angle, min_radius);
-//   min_path.strokeWidth = min_strokewidth;
-//   min_path.strokeColor = 'green';
-//
-//   var sec_path = drawArc(center, sec_angle, sec_radius);
-//   sec_path.strokeWidth = sec_strokewidth;
-//   sec_path.strokeColor = 'blue';
-
-//   {
-//   var radius_1 = 100;
-//   var center_1 = new Point(300,200);
-//
-//   var circle_1 = new Path.Circle(center_1, radius_1);
-//   circle_1.strokeWidth = 30;
-//   circle_1.strokeColor = 'red';
-//
-//   var circle_2 = drawArc(center_1, 270, radius_1);
-//   circle_2.strokeWidth = 30;
-//   circle_2.strokeColor = 'green';
-//   }
-
-//   {
-//     var radius_1 = 200;
-//     var center_1 = new Point(300,400);
-//
-//     var arc = drawArc(center_1, 90, radius_1);
-//     arc.strokeWidth = 40;
-//     arc.strokeColor = 'green';
-//   }
-
-//   {
-//     var radius_1 = 240;
-//     var center_1 = new Point(300,400);
-//
-//     var circle = new Path.Circle(center_1, radius_1);
-//     circle.strokeWidth = 40;
-//     circle.strokeColor = 'pink';
-//   }
-
-  /*
-  var ct = new Point(300, 400);
-  var rd = 230;
-  var sw = 100;
-
-  {
-    var circle = new Path.Circle(ct, rd);
-    circle.strokeWidth = sw;
-    circle.strokeColor = 'green';
-  }
-
-  {
-    rd += sw;
-
-    var circle = new Path.Circle(ct, rd);
-    circle.strokeWidth = sw;
-    circle.strokeColor = 'pink';
-  }
-  */
-
-  // var curvePath = new Path('M300,200 h-150 a150,150 0 1,0 150,-150 z');
-  // var curvePath = new Path('M300,200 h-150 a150,150 0 1,0 150,-150 z');
-  var cp1 = new Path('M150,200 a150,150 0 1,0 150,-150');
-  cp1.strokeWidth = 40;
-  cp1.strokeColor = 'red';
-
-  var cp2 = new Path('M110,200 a190,190 0 1,0 190,-190');
-  cp2.strokeWidth = 40;
-  cp2.strokeColor = 'blue';
+  // hands_layer.addChildren([ hor_hand.arc, hor_hand.tail, hor_hand.head, hor_hand.text
+  //                         , min_hand.arc, min_hand.tail, min_hand.head, min_hand.text
+  //                         , sec_hand.arc, sec_hand.tail, sec_hand.head, sec_hand.text
+  //                         ]);
 
   paper.view.update();
 
