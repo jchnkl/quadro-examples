ea1cc10ab88fb06b848d82e629ea22262c9a2e71
Jochen Keil <jochen.keil@gmail.com>
2015-07-14 11:52:25 +0200

    The global dbus object is now uppercase: DBus

--- org.freedesktop.UPower.js
+++ org.freedesktop.UPower.js
@@ -7,7 +7,7 @@ function upowerOnBattery()
   var method = 'Get';
   var arg1   = 'org.freedesktop.UPower';
   var arg2   = 'OnBattery';
-  return dbus.system.call(upowerService, path, iface, method, arg1, arg2);
+  return DBus.system.call(upowerService, path, iface, method, arg1, arg2);
 }
 
 function upowerGetDeviceProperty(devicePath, property)
@@ -15,7 +15,7 @@ function upowerGetDeviceProperty(devicePath, property)
   var iface  = 'org.freedesktop.DBus.Properties';
   var method = 'Get';
   var arg1   = 'org.freedesktop.UPower.Device';
-  return dbus.system.call(upowerService, devicePath, iface, method, arg1, property);
+  return DBus.system.call(upowerService, devicePath, iface, method, arg1, property);
 }
 
 function upowerGetDeviceProperties(devicePath)
@@ -23,5 +23,5 @@ function upowerGetDeviceProperties(devicePath)
   var iface  = 'org.freedesktop.DBus.Properties';
   var method = 'GetAll';
   var arg1   = 'org.freedesktop.UPower.Device';
-  return dbus.system.call(upowerService, devicePath, iface, method, arg1);
+  return DBus.system.call(upowerService, devicePath, iface, method, arg1);
 }
