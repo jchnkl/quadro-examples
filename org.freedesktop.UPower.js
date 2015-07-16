c0569c4171b0e65e6ad46e2a19e4b33af0a04d24
Jochen Keil <jochen.keil@gmail.com>
2015-07-16 12:53:07 +0200

    Add new UPower DBus methods

--- org.freedesktop.UPower.js
+++ org.freedesktop.UPower.js
@@ -1,5 +1,17 @@
 var upowerService =  'org.freedesktop.UPower';
 
+var Type = {
+  Unknown:    0,
+  LinePower:  1,
+  Battery:    2,
+  Ups:        3,
+  Monitor:    4,
+  Mouse:      5,
+  Keyboard:   6,
+  Pda:        7,
+  Phone:      8
+};
+
 function upowerOnBattery()
 {
   var path   = '/org/freedesktop/UPower';
@@ -24,4 +36,22 @@ function upowerGetDeviceProperties(devicePath)
   var method = 'GetAll';
   var arg1   = 'org.freedesktop.UPower.Device';
   return DBus.system.call(upowerService, devicePath, iface, method, arg1);
+}
+
+function upowerEnumerateDevices()
+{
+  var iface  = 'org.freedesktop.UPower';
+  var path   = '/org/freedesktop/UPower';
+  var method = 'EnumerateDevices';
+  return DBus.system.call(upowerService, path, iface, method);
+}
+
+function upowerGetNativePath(devicePath)
+{
+  return upowerGetDeviceProperty(devicePath, 'NativePath');
+}
+
+function upowerGetType(devicePath)
+{
+  return upowerGetDeviceProperty(devicePath, 'Type');
 }
