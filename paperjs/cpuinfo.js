59856c3b274b573fccba8b0a45ebf61b71b80967
Jochen Keil <jochen.keil@gmail.com>
2015-07-19 12:18:20 +0200

    Rewrite as object, make configuration easier

--- paperjs/cpuinfo.js
+++ paperjs/cpuinfo.js
@@ -1,107 +1,120 @@
-var columns = 4;
-
-var gap = 10;
-var radius = 35;
-var strokeWidth = 25;
-var strokeColor = '#85b3c4';
-
-var fontFamily = 'Ubuntu Light';
-var fontColor = '#85b3c4';
-var fontSize = 15;
+var CpuInfo =
+  { main: main
+  , renderUsage: renderUsage
+  , getUsageInfos: getUsageInfos
+  };
 
 var multiCircle = window.common.multiCircle;
 
-function renderUsageLayers(bottomColor, color, infos)
+function padConfig(defaultConfig, partialConfig)
 {
-  return  multiCircle({ infos: infos.reverse()
-                      , center: { x: view.size.width / 2, y: view.size.height / 2 }
-                      , innerRadius: 0.5 * view.size.width / 4
-                      , outerRadius: view.size.width / 4
-                      , gap: 2
-                      , color: color
-                      , opacity: 1.0
-                      , baseColor: bottomColor
-                      , baseOpacity: 0.4
-                      , fontColor: fontColor
-                      , fontFamily: fontFamily
+  if (partialConfig == null) {
+    return defaultConfig;
+  }
+
+  for (var p in defaultConfig) {
+    if (partialConfig[p] == null) {
+      partialConfig[p] = defaultConfig[p];
+    }
+  }
+  return partialConfig;
+}
+
+function renderUsage(infos)
+{
+  return  multiCircle({ infos:         infos.reverse()
+                      , center:        this.config.center
+                      , innerRadius:   this.config.innerRadius
+                      , outerRadius:   this.config.outerRadius
+
+                      , circleGap:     this.config.circleGap
+
+                      , statusColor:   this.config.statusColor
+                      , statusCpacity: this.config.statusCpacity
+
+                      , fontFamily:    this.config.fontFamily
+                      , fontColor:     this.config.fontColor
+                      , fontSize:      this.config.fontSize
+
+                      , baseColor:     this.config.baseColor
+                      , baseOpacity:   this.config.baseOpacity
                       });
 }
 
-
-function main()
+function getUsageInfos(getCoreUsage)
 {
-  var last = [];
+  var infos = [];
 
-  for (var i = 0; i < 8; ++i) {
-    last[i] = coreInfo(i);
-  }
+  for (var n = 0; n < this.ncpus; ++n) {
+      var usage = getCoreUsage(n);
 
-  var ncpus = getNumCpus();
-
-  var bottomColor = new Color(strokeColor).convert('hsb')
-  bottomColor.saturation = 0.2;
-
-  var layer = new Layer();
-
-  {
-    var infos = [];
-
-    for (var i = 0; i < ncpus; ++i) {
-        var usage = cpuUsagePercentSinceBoot(coreInfo(i));
-
-        var color = new Color(strokeColor);
-        color.red   += 0.4 * usage / 100;
-        color.green -= 0.2 * usage / 100;
-        color.blue  -= 0.2 * usage / 100;
-
-        infos.push({ percent: usage
-                   , color: color
-                   , text: 'Core ' + i + '    ' + usage.toFixed(0) + '%'
-                   });
-    }
-
-    layer.addChild(renderUsageLayers(bottomColor, color, infos));
-  }
-
-  setInterval(function() {
-    layer.removeChildren();
-
-    var infos = [];
-
-    for (var i = 0; i < ncpus; ++i) {
-      var now = coreInfo(i);
-      var usage = cpuUsagePercent(last[i], now);
-
-      var color = new Color(strokeColor);
+      var color = new Color(this.statusColor);
       color.red   += 0.4 * usage / 100;
       color.green -= 0.2 * usage / 100;
       color.blue  -= 0.2 * usage / 100;
 
       infos.push({ percent: usage
                  , color: color
-                 , text: 'Core ' + i + '    ' + usage.toFixed(0) + '%'
+                 , text: 'Core ' + n + '    ' + usage.toFixed(0) + '%'
                  });
+  }
 
-      last[i] = now;
-    }
-
-    layer = multiCircle({ infos: infos.reverse()
-                        , center: { x: view.size.width / 2, y: view.size.height / 2 }
-                        , innerRadius: 0.5 * view.size.width / 4
-                        , outerRadius: view.size.width / 4
-                        , gap: 2
-                        , color: color
-                        , opacity: 1.0
-                        , baseColor: bottomColor
-                        , baseOpacity: 0.4
-                        , fontColor: fontColor
-                        , fontFamily: fontFamily
-                        });
-
-
-    paper.view.update();
-
-  }, 3000);
+  return infos;
 }
 
-main();
+function main()
+{
+  var defaultConfig =
+    { center: { x: view.size.width / 2, y: view.size.height / 2 }
+
+    , innerRadius: 0.5 * view.size.width / 2
+    , outerRadius: view.size.width / 2
+
+    , circleGap:         10
+
+    , statusColor: '#85b3c4'
+    , statusCpacity: 1.0
+
+    , fontFamily:  'Ubuntu Light'
+    , fontColor:   '#85b3c4'
+    , fontSize:    15
+
+    , baseOpacity: 0.6
+    }
+
+  defaultConfig.baseColor = new Color(defaultConfig.statusColor).convert('hsb');
+  defaultConfig.baseColor.saturation = 0.2;
+
+  var self = this;
+
+  self.config = padConfig(defaultConfig, null);
+  self.ncpus = getNumCpus();
+  self.last = [];
+
+  for (var n = 0; n < 8; ++n) {
+    self.last[n] = coreInfo(n);
+  }
+
+  var infos = self.getUsageInfos(function(n) {
+    return cpuUsagePercentSinceBoot(coreInfo(n));
+  });
+
+  self.layer = self.renderUsage(infos);
+
+  setInterval(function() {
+    self.layer.removeChildren();
+
+    var infos = self.getUsageInfos(function(n) {
+      var now = coreInfo(n);
+      old = self.last[n];
+      self.last[n] = now;
+      return cpuUsagePercent(old, now);
+    });
+
+    layer = self.renderUsage(infos);
+
+    paper.view.update();
+  }, 3000)
+}
+
+CpuInfo.main();
